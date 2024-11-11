ca03bab59f2e708235800b21aa8240954f1fa0aa
janguk <janguk95@naver.com>
2024-11-11 14:58:35 +0900

    sh 1111

--- SSAFY_tong/finalPJT/src/main/java/com/ssafy/tong/board/config/DBConfig.java
+++ SSAFY_tong/finalPJT/src/main/java/com/ssafy/tong/board/config/DBConfig.java
@@ -8,7 +8,8 @@ import org.springframework.context.annotation.Configuration;
 		"com.ssafy.tong.board.model.dao", 
 		"com.ssafy.tong.comment.model.dao", 
 		"com.ssafy.tong.reply.model.dao", 
-		"com.ssafy.tong.expert.model.dao"
+		"com.ssafy.tong.expert.model.dao",
+		"com.ssafy.tong.user.model.dao"
 })
 public class DBConfig {
 	
---
+++ SSAFY_tong/finalPJT/src/main/java/com/ssafy/tong/user/model/service/UserServiceImpl.java
@@ -0,0 +1,59 @@
+package com.ssafy.tong.user.model.service;
+
+import org.springframework.stereotype.Service;
+
+import com.ssafy.tong.user.model.User;
+import com.ssafy.tong.user.model.dao.UserDao;
+
+@Service
+public class UserServiceImpl implements UserService {
+	private final UserDao userDao;
+	public UserServiceImpl(UserDao userDao) {
+		this.userDao = userDao;
+	}
+
+	// 회원가입
+	@Override
+	public void signUp(User user) {
+		userDao.signUp(user);
+	}
+
+	// 로그인
+	@Override
+	public User signIn(String userId, String password) {
+		// 사용자 정보 조회
+        User user = userDao.signIn(userId);
+
+        // 사용자 존재 여부 및 비밀번호 확인
+        if (user != null && password.equals(user.getPassword())) {
+            return user; // 로그인 성공
+        }
+
+        return null; // 로그인 실패
+	}
+
+	// 비밀번호 변경 
+	@Override
+	public void updatePassword(User loggedInUser) {
+		userDao.updatePassword(loggedInUser);
+	}
+	
+	
+	// 카카오 회원이 이미 가입 되었는지 확인 위해서
+	@Override
+	public User findUserByUserId(String kakaoId) {
+		// 사용자 정보 조회
+        User user = userDao.signIn(kakaoId);
+		
+        // 카카오 회원으로 이미 가입 했었음
+        if (user != null) {
+        	return user;
+        }
+		return null; // 카카오 회원으로 가입 안함
+	}
+
+
+
+
+
+}
